f8b8b1cecf64a3670499a330946614874af3fc53
Julio Gonzalez <jrgonzalezrios@gmail.com>
2021-12-29 20:11:54 -0600

    Add post route to pay a job

--- src/app.js
+++ src/app.js
@@ -68,4 +68,62 @@ app.get('/jobs/unpaid', getProfile, async (req, res) => {
     if(!jobsUnpaid) return res.status(404).end()
     res.json(jobsUnpaid)
 })
+
+app.post('/jobs/:job_id/pay', getProfile, async (req, res) => {
+    
+
+    const { Job, Contract, Profile } = req.app.get('models')
+    const { id, type, balance } = req.profile
+    const {job_id} = req.params
+    // Verify type of user and balance
+    if (type !== 'client') {
+        const errResp = {
+            message: "Invalid Client"
+        }
+        return res.status(400).json(errResp)
+    }
+    if (balance <= 0) {
+        const errResp = {
+            message: "Insufficient funds"
+        }
+        return res.status(400).json(errResp)
+    }
+    // Deposit money move from client to contracts
+    const job = await Job.findOne({ where: {
+            id: job_id,
+            price:{ [Op.lte]: balance },
+            paid: {[Op.is]: null}
+        },include:{
+            model: Contract
+        }
+    })
+    console.log(job)
+    const resp = {}
+    if(!job) {
+        resp.message = 'Invalid Job, can not be paid or was already paid'
+        return res.status(400).json(resp)
+    }
+    // Setting new Balance
+    job.paid = 1
+    job.paymentDate = new Date()
+    jobUpdated = await job.save()
+
+    // Update client balance
+    const client = await Profile.findOne({where: {id: id}})
+    client.balance -= job.price
+    await client.save()
+
+    // Update contractor balance
+    const contractor = await Profile.findOne({where: {id: job.Contract.ContractorId}})
+    contractor.balance += job.price
+    await contractor.save()
+
+    if(jobUpdated){
+        // update Contractor's balance
+        resp.message = `Job ${job_id} was succesfuly paid, Client Balance: ${client.balance}` 
+        return res.json(resp)
+    }
+    
+
+})
 module.exports = app;
